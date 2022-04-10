ae1cae876c37502d9208d65f9b71143ad13dd5b3
TiagoMerc <tmrrosario9@gmail.com>
2022-04-10 20:09:58 -0300

    Modificações no projeto/jogo

---
+++ README.md
@@ -0,0 +1,24 @@
+# DIO---Recriando-o-famoso-jogo-do-dinossauro-sem-internet
+
+### Digital Innovation One [DIO](https://www.dio.me/)
+
+### BootCamp - Órbi Web Games Developer
+
+**DESCRIÇÃO:**
+Neste projeto você terá o desafio de criar uma cópia do jogo do dinossauro, famoso no Chrome por nos entreter quando estamos sem conexão. Nele iremos utilizar HTML, CSS e JavaScript, abordando de maneira simples diversos conceitos introdutórios importantes para programação na web como tags básicas de HTML, manipulação de eventos, funções e manipulação de elementos HTML usando JavaScript, estilização e animações básicas com CSS.
+
+**HTML, CSS e JavaScript**
+
+Front-End | Intermediário
+
+**ESPECIALISTA:** [Celso Henrique](https://github.com/celso-henrique/)
+_________
+
+#### [Repositório Base](https://github.com/TiagoMerc/DIO---Recriando-o-famoso-jogo-do-dinossauro-sem-internet.git)
+
+
+#### Modificações:
+
+- Foi inserido informações no arquivo index, como instruções de jogo e referẽncias do projeto.
+
+- Possibilidade do usuário clicar na mensagem de Game Over "Fim de jogo  (-_-) | Jogar novamente!" para reiniciar o jogo.
---
+++ script.js
@@ -0,0 +1,75 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+let isGameOver = false;
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if(!isJumping) {
+      jump();
+    }
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval);
+      //Descer
+      let downInterval = setInterval(() => {
+        if(position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      },  20); 
+    } else {
+      //subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
+
+//Criando os cactus
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  if(isGameOver) return;
+
+  cactus.classList.add('cactus');
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosition + 'px';
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosition < -60) {
+
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      //Game Over - Perdeu o jogo
+      clearInterval(leftTimer);
+      isGameOver = true;
+     
+      document.body.innerHTML = '<a href="./index.html" class="game-over">Fim de jogo  (-_-) | Jogar novamente!</a>';
+      
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime); //Recursividade
+} 
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
+// Site para descobrir as informações do código tecla digitada/evento
